ac9e6cd1d28345a755f1b4fcf8a937e518168270
wyx <2297882178wyx@gmail.com>
2026-04-04 16:45:06 +0800

    theme name modify

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -25,7 +25,7 @@ return {
     },
     opts = {
       options = {
-        theme = "catppuccin",
+        theme = "catppuccin-nvim",
         always_divide_middle = false,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
